84e8fc486d43966862c2e79acf32bb66f1681788
kharann <anhkhav@gmail.com>
2019-12-20 15:36:39 +0100

    Added a JUnit Pizza Test and added JUnit; Split PizzaType from Pizza since only PizzaType is stored

--- backend/src/main/kotlin/backend/models/pizza/Pizza.kt
+++ backend/src/main/kotlin/backend/models/pizza/Pizza.kt
@@ -1,19 +1,15 @@
 package backend.models.pizza
 
-data class Pizza(
-  val name: String? = null,
-  val sauce: Sauce = Sauce.TOMATO,
-  val cheese: Cheese = Cheese.MOZERELLA,
-  val toppings: List<Topping> = listOf(),
-  val id: Int = 0
-) {
+import backend.models.order.Dough
+import backend.models.order.Size
+
+class Pizza(private val pizzaType: PizzaType, private val dough: Dough, val size: Size) {
 
   fun getPrice(): Int {
-    return sauce.value + cheese.value +
-      toppings.fold(0) { sum, topping -> sum + topping.value }
+    return size.value + pizzaType.value
   }
 
   override fun toString(): String {
-    return "name: $name, sauce: $sauce, cheese: $cheese, toppings: $toppings"
+    return "size: $size dough: $dough pizza: $pizzaType price: ${getPrice()}"
   }
 }
